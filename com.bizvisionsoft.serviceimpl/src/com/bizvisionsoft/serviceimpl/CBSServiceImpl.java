04c52a5041577104fb0d9dd2ce6d301c98555c86
hua <hua@hua17>
2018-04-23 16:13:39 +0800

    Signed-off-by: hua <hua@hua17>

--- com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/CBSServiceImpl.java
+++ com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/CBSServiceImpl.java
@@ -1,16 +1,56 @@
 package com.bizvisionsoft.serviceimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import com.bizvisionsoft.service.CBSService;
 import com.bizvisionsoft.service.model.CBSItem;
 import com.bizvisionsoft.service.model.CBSPeriod;
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Field;
 
 public class CBSServiceImpl extends BasicServiceImpl implements CBSService {
+
+	/**
+	 * 
+	 * @return
+	 */
+	public List<CBSItem> query(BasicDBObject match) {
+		/*
+		 * db.getCollection('cbs').aggregate([
+		 * {$lookup:{"from":"cbsPeriod","localField":"_id","foreignField":"cbsItem_id",
+		 * "as":"_period"}},
+		 * 
+		 * {$addFields:{_budget: {$map:{"input":"$_period","as":"itm","in":{"k":
+		 * "$$itm.id","v":"$$itm.budget"}}}}},
+		 * 
+		 * {$addFields:{"budgetTotal":{$sum:"$_budget.v"}}},
+		 * 
+		 * {$addFields:{"budget":{$arrayToObject: "$_budget" }}},
+		 * 
+		 * {$project:{"_period":false,"_budget":false}} ])
+		 */
+		List<Bson> pipeline = new ArrayList<Bson>();
+		if (match != null)
+			pipeline.add(Aggregates.match(match));
+		pipeline.add(Aggregates.lookup("cbsPeriod", "_id", "cbsItem_id", "_period"));
+		pipeline.add(Aggregates.addFields(//
+				new Field<BasicDBObject>("_budget",
+						new BasicDBObject("$map",
+								new BasicDBObject("input", "$_period").append("as", "itm").append("in",
+										new BasicDBObject("k", "$$itm.id").append("v", "$$itm.budget")))),
+				new Field<BasicDBObject>("budgetTotal", new BasicDBObject("$sum", "$_budget.v")),
+				new Field<BasicDBObject>("budget", new BasicDBObject("$arrayToObject", "$_budget"))));
+		pipeline.add(Aggregates.project(new BasicDBObject("_period", false).append("_budget", false)));
+		ArrayList<CBSItem> result = new ArrayList<CBSItem>();
+		Service.col(CBSItem.class).aggregate(pipeline).into(result);
+		return result;
+	}
 
 	/**
 	 * db.getCollection('accountItem').aggregate([ {$match:{parent_id:null}},
