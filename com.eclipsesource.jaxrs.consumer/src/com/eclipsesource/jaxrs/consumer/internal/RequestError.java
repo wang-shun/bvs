3af252eb5f9f584247137d52100ae0bbb5e666be
hua <hua@10.254.33.103>
2018-05-16 10:16:49 +0800

    Signed-off-by: hua <hua@10.254.33.103>

--- com.eclipsesource.jaxrs.consumer/src/com/eclipsesource/jaxrs/consumer/internal/RequestError.java
+++ com.eclipsesource.jaxrs.consumer/src/com/eclipsesource/jaxrs/consumer/internal/RequestError.java
@@ -3,60 +3,62 @@ package com.eclipsesource.jaxrs.consumer.internal;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
-
 public class RequestError {
 
-  private final String requestUrl;
-  private final Response response;
-  private final String method;
-  private final String entity;
+	private final String requestUrl;
+	private final Response response;
+	private final String method;
+	private final String entity;
 
-  public RequestError( RequestConfigurer configurer, Response response, String method ) {
-    this.response = response;
-    this.method = method;
-    this.requestUrl = configurer.getRequestUrl();
-    this.entity = getInternalEntity();
-  }
-  
-  public String getInternalEntity() {
-    if( response.hasEntity() ) {
-      return response.readEntity( String.class );
-    }
-    return null;
-  }
+	public RequestError(RequestConfigurer configurer, Response response, String method) {
+		this.response = response;
+		this.method = method;
+		this.requestUrl = configurer.getRequestUrl();
+		this.entity = getInternalEntity();
+	}
 
-  public int getStatus() {
-    return response.getStatus();
-  }
+	public String getInternalEntity() {
+		if (response.hasEntity()) {
+			return response.readEntity(String.class);
+		}
+		return null;
+	}
 
-  public String getMessage() {
-	  return entity.toString();
-//    StringBuilder stringBuilder = new StringBuilder();
-//    stringBuilder.append( "Failed to send " + method + " request to: " + requestUrl );
-//    stringBuilder.append( "\n" );
-//    stringBuilder.append( "Received Status: " + response.getStatus() );
-//    if( entity != null ) {
-//      stringBuilder.append( "\nReceived Body: " );
-//      stringBuilder.append( entity );
-//    }
-//    stringBuilder.append( "\n" );
-//    return stringBuilder.toString();
-  }
-  
-  public String getEntity() {
-    return entity;
-  }
+	public int getStatus() {
+		return response.getStatus();
+	}
 
-  public String getMethod() {
-    return method;
-  }
-  
-  public String getRequestUrl() {
-    return requestUrl;
-  }
-  
-  public MultivaluedMap<String, Object> getHeaders() {
-    return response.getHeaders();
-  }
-  
+	public String getMessage() {
+		if (entity != null) {
+			return entity.toString();
+		} else {
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.append("服务" + method + " 请求: " + requestUrl);
+			stringBuilder.append("\n");
+			stringBuilder.append("接收到状态代码为: " + response.getStatus());
+			if (entity != null) {
+				stringBuilder.append("\n消息体: ");
+				stringBuilder.append(entity);
+			}
+			stringBuilder.append("\n");
+			return stringBuilder.toString();
+		}
+	}
+
+	public String getEntity() {
+		return entity;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public String getRequestUrl() {
+		return requestUrl;
+	}
+
+	public MultivaluedMap<String, Object> getHeaders() {
+		return response.getHeaders();
+	}
+
 }
