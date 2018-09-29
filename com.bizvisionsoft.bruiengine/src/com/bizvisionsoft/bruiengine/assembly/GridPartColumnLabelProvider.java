49e77122d99f4611536ee3bcad437116ba6b8c10
hua <ghuazh@gmail.com>
2018-09-30 07:52:46 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.bruiengine/src/com/bizvisionsoft/bruiengine/assembly/GridPartColumnLabelProvider.java
+++ com.bizvisionsoft.bruiengine/src/com/bizvisionsoft/bruiengine/assembly/GridPartColumnLabelProvider.java
@@ -2,11 +2,9 @@ package com.bizvisionsoft.bruiengine.assembly;
 
 import org.eclipse.jface.viewers.CellLabelProvider;
 import org.eclipse.jface.viewers.ViewerCell;
-import org.eclipse.rap.rwt.RWT;
 
 import com.bizvisionsoft.bruicommons.model.Column;
 import com.bizvisionsoft.bruiengine.BruiGridRenderEngine;
-import com.bizvisionsoft.service.tools.Util;
 
 public class GridPartColumnLabelProvider extends CellLabelProvider {
 
@@ -25,18 +23,7 @@ public class GridPartColumnLabelProvider extends CellLabelProvider {
 	public void update(ViewerCell cell) {
 		renderEngine.renderCell(cell, column);
 	}
-
-	public String getColumnValue(Object element) {
-		// TODO 缺少调用注解时获取值的方法
-		Object value = renderEngine.getColumnValue(element, column);
-		String text;
-		if ((value instanceof Number) && ((Number) value).doubleValue() == 0 && !column.isForceDisplayZero()) {
-			text = "";
-		} else {
-			String format = column.getFormat();
-			text = Util.getFormatText(value, format, RWT.getLocale());
-		}
-		return text;
-	}
+	
+	
 
 }
