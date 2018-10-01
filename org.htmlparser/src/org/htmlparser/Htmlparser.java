cf95f34125f92e8e44e535161b10bb1673e3f5e0
hua <ghuazh@gmail.com>
2018-10-01 10:01:28 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

---
+++ org.htmlparser/src/org/htmlparser/Htmlparser.java
@@ -0,0 +1,33 @@
+package org.htmlparser;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Htmlparser implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Htmlparser.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Htmlparser.context = null;
+	}
+}
