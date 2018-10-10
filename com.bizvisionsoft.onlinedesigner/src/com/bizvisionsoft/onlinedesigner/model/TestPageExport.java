fec9c81f3c3a5aa425c032b5dc00cf64f9165f46
hua <ghuazh@gmail.com>
2018-10-10 23:43:04 +0800

    POI 分页导出Excel文件

---
+++ com.bizvisionsoft.onlinedesigner/src/com/bizvisionsoft/onlinedesigner/model/TestPageExport.java
@@ -0,0 +1,14 @@
+package com.bizvisionsoft.onlinedesigner.model;
+
+import org.apache.poi.bizvisionsoft.excel.Demo;
+
+import com.bizvisionsoft.annotations.ui.common.Execute;
+
+public class TestPageExport {
+
+	@Execute
+	public void execute() {
+		
+		new Demo().test();
+	}
+}
