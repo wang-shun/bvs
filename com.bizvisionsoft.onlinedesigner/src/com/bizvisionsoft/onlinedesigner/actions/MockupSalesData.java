7e34da957048bbdcd79d746b0c5e41099f314776
hua <ghuazh@gmail.com>
2018-10-20 08:53:36 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.onlinedesigner/src/com/bizvisionsoft/onlinedesigner/actions/MockupSalesData.java
+++ com.bizvisionsoft.onlinedesigner/src/com/bizvisionsoft/onlinedesigner/actions/MockupSalesData.java
@@ -1,14 +1,12 @@
 package com.bizvisionsoft.onlinedesigner.actions;
 
 import com.bizvisionsoft.annotations.ui.common.Execute;
-import com.bizvisionsoft.service.CommonService;
-import com.bizvisionsoft.serviceconsumer.Services;
 
 public class MockupSalesData {
 	
 	@Execute
 	public void execute() {
-		Services.get(CommonService.class).mockupSalesData();
+//		Services.get(CommonService.class).mockupSalesData();
 	}
 
 }
