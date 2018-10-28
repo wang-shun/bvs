b9f722acee6de3dac2e2197a41e751225f02650f
hua <ghuazh@gmail.com>
2018-10-28 22:35:15 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

---
+++ com.bizvisionsoft.bruiengine/src/com/bizvisionsoft/bruiengine/ui/setting/PageEditor.java
@@ -0,0 +1,28 @@
+package com.bizvisionsoft.bruiengine.ui.setting;
+
+import java.util.Optional;
+
+import org.eclipse.swt.widgets.Display;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bizvisionsoft.bruicommons.model.Page;
+import com.bizvisionsoft.bruiengine.ui.View;
+
+public class PageEditor {
+	
+	public static Logger logger = LoggerFactory.getLogger(PageEditor.class);
+
+	public static void open() {
+		Optional.ofNullable(Display.getCurrent().getActiveShell()).map(s -> s.getData("part")).ifPresent(o -> {
+			if (o instanceof View)
+				openPart((View) o);
+		});
+	}
+
+	public static void openPart(View part) {
+		Page page = part.getPage();
+		logger.debug(page.getName()+" ["+page.getId()+"]");
+	}
+
+}
